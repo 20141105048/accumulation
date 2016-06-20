60abecedf4025026f67b3b46b57515efd0901a2e
l20141105048 <l20141105048@King.local>
2016-06-20 10:09:53 +0800

    累加 第一次提交
    用swift语句实现从1-100的累加，首先定义一个变量a，将其赋值为0，调用for循环语句，实现i由1递增到100，再用语句a＝a+i实现1-
    100的累加，程序的运行结果为5050.

---
+++ main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  accumulation
+//
+//  Created by l20141105048 on 16/6/20.
+//  Copyright © 2016年 l20141105048. All rights reserved.
+//
+
+import Foundation
+
+var a=0
+for (var i=0; i<=100; i++){
+    a=i+a
+    
+    
+
+
+}
+print("a=\(a)")
+
